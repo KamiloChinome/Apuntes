36e67a052f18c6334bd21bdbfe936b9102ae2d78
Kamilo Chinome <Kamiloandresch@gmail.com>
2022-09-17 23:44:06 -0500

    segundo commit

--- lib/scrolls.dart
+++ lib/scrolls.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 // PageView: scroll fuerte, recomendado para cambiar vistas de pantalla completamente
 Widget build(BuildContext context) {
-  return  Scaffold(
-    body: PageView(
-      physics: const BouncingScrollPhysics(), //Permite añadir scroll que te saca de la p antalla(efectos elegantes)
-      scrollDirection: Axis.vertical, //direccion del scroll
-      children: const [
-        //Cambios de vista
-        Page1(),
-        Page2(),
-        Page3(),
-        Page4(),
-      ],
+  return  Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(), //Permite añadir scroll que te saca de la p antalla(efectos elegantes)
+        scrollDirection: Axis.vertical, //direccion del scroll
+        children: const [
+          //Cambios de vista
+          // Page1(),
+          // Page2(),
+          // Page3(),
+          // Page4(),
+        ],
+      ),
     ),
   );
 }
